ca966410adecaaed72deb5e9dab570f11a1be4d3
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2023-01-15 15:16:23 +0530

    setup basic views in project and app level

--- base/views.py
+++ base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def home(request):
+    return render(request, 'base/home.html')
+
+def room(request):
+    return render(request, 'base/room.html')
+
