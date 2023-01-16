3a60cc6cd67416bc60b8ce30ec7e8c8eb235894f
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2023-01-16 21:23:34 +0530

    search by host name

--- base/views.py
+++ base/views.py
@@ -10,7 +10,7 @@ def home(request):
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
-        Q(description__icontains=q)
+        Q(host__username__icontains=q)
     )
     topics = Topic.objects.all()
     context = {
