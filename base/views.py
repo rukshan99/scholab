fd9ab22d5b3a62cc253841cbaa2f9baf82bf9e75
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2023-01-21 21:58:01 +0530

    implement send message functionality

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 def loginView(request):
@@ -73,6 +73,14 @@ def home(request):
 def room(request, id):
     room = Room.objects.get(id=id)
     messages = room.message_set.all().order_by('-created')
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room_view', id=room.id)
     context = {
         'room': room,
         'room_messages': messages
