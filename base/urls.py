1664a5a11da58ddb5b3ac68792293b98f8edf2a8
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2023-01-21 21:12:57 +0530

    implement register functionality and page

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home_view'),
+    
     path('login/', views.loginView, name='login_view'),
     path('logout/', views.logoutView, name='logout_view'),
+    path('register/', views.registerView, name='register_view'),
+
     path('room/<str:id>/', views.room, name='room_view'),
     path('create-room/', views.createRoom, name='create_room_view'),
     path('update-room/<str:id>/', views.updateRoom, name='update_room_view'),
--- base/views.py
+++ base/views.py
@@ -5,15 +5,17 @@ from django.db.models import Q
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
 def loginView(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home_view')
-        
+
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
@@ -26,12 +28,31 @@ def loginView(request):
         except:
             messages.error(request, 'Authentication failed. Please check the credentials again.')
        
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutView(request):
     logout(request)
     return redirect('home_view')
+
+def registerView(request):
+    page = 'register'
+    form = UserCreationForm()
+    context = {
+        'page': page,
+        'form': form
+    }
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home_view')
+        else:
+            messages.error(request, 'An error occured during registration.')
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') or ''
