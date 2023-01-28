a62caeab7c442f06b7467df9920738d82c1b9e13
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2023-01-28 22:26:12 +0530

    apply theme in create room page, add functionality to create new topic when creating room

--- base/views.py
+++ base/views.py
@@ -110,31 +110,44 @@ def profileView(request, id):
 @login_required(login_url='login_view')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.participants.add(request.user)
-            room.save()
-            return redirect('home_view')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room = Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description')
+        )
+        room.participants.add(request.user)
+        room.save()
+        return redirect('home_view')
+    context = {
+        'form': form,
+        'topics': topics
+    }
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login_view')
 def updateRoom(request, id):
     room = Room.objects.get(id=id)
-    form = RoomForm(instance=room)
-
     if request.user != room.host:
         return HttpResponse('You are not allowed to update this room.')
+    
+    form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
             return redirect('home_view')
-    context = {'form': form}
+    context = {
+        'form': form,
+        'topics': topics,
+        'room': room
+    }
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login_view')
