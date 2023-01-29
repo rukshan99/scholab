23613b36e17a61cce94658a357f955d16c9f884a
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2023-01-29 14:57:23 +0530

    implement a serializer for room model

--- base/api/views.py
+++ base/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..models import Room
+from .serializers import RoomSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -14,4 +15,5 @@ def getRoutes(request):
 @api_view(['GET'])
 def getRooms(request):
     rooms = Room.objects.all()
-    return Response(rooms)
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
