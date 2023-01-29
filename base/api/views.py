73e753e1346c88c9ec09bb1ce7479f33f23f8826
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2023-01-29 14:51:58 +0530

    implemnt getRooms api

--- base/api/views.py
+++ base/api/views.py
@@ -1,8 +1,17 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from ..models import Room
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
+        'GET /api',
         'GET /api/rooms',
         'GET /api/rooms/:id'
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    return Response(rooms)
